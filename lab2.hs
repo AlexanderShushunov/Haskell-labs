807cf8805857041bca0a0087c4fb82b0a527e1c8
Alexandr Shushunov <ashushunov@mail.ru>
2017-04-28 10:28:24 +0300

    toDigits returns [] if an input is 0. Fixed.

--- lab2.hs
+++ lab2.hs
@@ -11,8 +11,8 @@ eval xs = foldl (\x y -> y + (10 * x)) 0 xs
 -- ===================================
 
 toDigits :: Integer -> [Integer]
-toDigits x  | x == 0    = []
-            | otherwise = toDigits (x `div` 10) ++ [x `rem` 10] 
+toDigits x | x >= 0 && x < 10  = [x]
+           | otherwise         = toDigits (x `div` 10) ++ [x `rem` 10]
 
 -- ===================================
 -- Ex. 1
